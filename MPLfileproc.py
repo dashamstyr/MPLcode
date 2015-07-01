Fixed multiple bugs in SNR calculation, backscatter and extinction calculations, added INSUFFICIENT SIGNAL to layer types, added invert_profile fucntion to inversion tools to handle molecular regions with low SNR

MPLfileproc.py
@@ -1,13 +1,17 @@
+#from __future__ import absolute_import
+import os,glob,site
+home=os.environ['homepath']
+site.addsitedir('{0}\\Dropbox\\Python_Scripts\\GIT_Repos\\'.format(home))
+
 import numpy as np
-import os,glob
-import MPLtools as mtools
-import MPLprocesstools as mproc
 import datetime
 import matplotlib.pyplot as plt
-import MPLplot as mplot
 import pandas as pan
 from scipy import signal
 
+import MPLtools as mtools
+import MPLprocesstools as mproc
+import MPLplot as mplot
 
 def fileproc(**kwargs):
 
@@ -35,7 +39,7 @@ def fileproc(**kwargs):
     
     """
     #general kwargs
-    rawfiles = kwargs.get('rawfiles',[])
+    rawfiles = kwargs.get('rawfiles',None)
     altrange = kwargs.get('altrange',np.arange(150,15000,30))        
     timestep = kwargs.get('timestep','60S')
     doplot = kwargs.get('doplot',False)
@@ -61,7 +65,7 @@ def fileproc(**kwargs):
     #findalllayers kwargs
     molthresh=kwargs.get('molthresh',1.0)
     layernoisethresh=kwargs.get('layernoisethresh',1.0)      
-    bg_alt=kwargs.get('bg_alt',[])
+    bg_alt=kwargs.get('bg_alt',None)
     datatype=kwargs.get('datatype','NRB')
     winsize=kwargs.get('winsize',5)
     wavelet=kwargs.get('wavelet',signal.ricker)
@@ -72,17 +76,17 @@ def fileproc(**kwargs):
     layerCWTrange=kwargs.get('layerCWTrange',np.arange(2,5))
     PBLwavelet=kwargs.get('PBLwavelet',mproc.dog)
     PBLCWTrange=kwargs.get('PBLCWTrange',np.arange(2,10))
-    sigma0=kwargs.get('sigma0',[])
+    sigma0=kwargs.get('sigma0',None)
     waterthresh=kwargs.get('waterthresh',0.10)
     icethresh=kwargs.get('icethresh',0.25)
     smokethresh=kwargs.get('smokethresh',0.05)
     dustthresh=kwargs.get('dustthresh',0.15)
     
     #correction kwargs
-    refalt=kwargs.get('refalt',[])
-    calrange=kwargs.get('calrange',[])
+    refalt=kwargs.get('refalt',None)
+    calrange=kwargs.get('calrange',None)
     method=kwargs.get('method','klett2')
-    lrat=kwargs.get('lrat',[])
+    lrat=kwargs.get('lrat',None)
     mode=kwargs.get('mode','copol')
     
     #plot kwargs
@@ -95,10 +99,11 @@ def fileproc(**kwargs):
     SNRmask = kwargs.get('SNRmask',False)
     SNRthresh=kwargs.get('SNRthresh',1.0)
     SNRtype=kwargs.get('SNRtype','NRB')
+    interpolate=kwargs.get('interpolate',None) #other methods include none, bilinear, gaussian and hermite
     #starttime and endtime are defined later      
     olddir=os.getcwd()
     
-    if len(rawfiles)==0:
+    if rawfiles is None:
         rawfiles = mtools.get_files('Select files to process',filetype = ('.mpl','*.mpl'))
     
     [path,startfile] = os.path.split(rawfiles[0])
@@ -162,48 +167,41 @@ def fileproc(**kwargs):
                 'topplot_limits':back_limits,'bottomplot_limits':ext_limits,
                 'hours':hours,'fsize':fsize,'savefilename':coefplotfilename,
                 'savefilepath':plotsavepath,'SNRmask':SNRmask,'SNRthresh':SNRthresh,
-                'SNRtype':SNRtype,'showplot':showplot,'saveplot':saveplot}
+                'interpolate':interpolate,'SNRtype':SNRtype,'showplot':showplot,'saveplot':saveplot}
     corkwargs={'refalt':refalt,'calrange':calrange,'method':method,'lrat':lrat,'mode':mode}
     
     plotfilename = '{0}.png'.format(d_filename.split('.')[0])
     plotkwargs={'altrange':altrange,'topplot_limits':NRB_limits,'bottomplot_limits':depol_limits,
                 'hours':hours,'fsize':fsize,'savefilename':plotfilename,'savefilepath':plotsavepath,
-                'SNRmask':SNRmask,'SNRthresh':SNRthresh,'SNRtype':SNRtype,
+                'SNRmask':SNRmask,'SNRthresh':SNRthresh,'SNRtype':SNRtype,'interpolate':interpolate,
                 'showplot':showplot,'saveplot':saveplot}
     if NRBmask:
         NRBmaskkwargs={'NRBmasktype':NRBmasktype,'NRBthresh':NRBthresh,'NRBmin':NRBmin,
                        'NRBminalt':NRBminalt,'NRBnumprofs':NRBnumprofs,'inplace':NRBinplace}
-        MPLdat_masked=mproc.NRB_mask_all(MPLdat_event,**NRBmaskkwargs)
+        MPLdat_event=mtools.NRB_mask_all(MPLdat_event,**NRBmaskkwargs)
 #        calrange=mproc.findcalrange(MPLdat_masked)
-        if docorrection:            
-            MPLdat_masked=mproc.basiccorrection(MPLdat_masked,**corkwargs)
-            if docorplot:
-                mplot.doubleplot(MPLdat_masked,plotfilename=coefplotfilename,**coefplotkwargs)
-        
-        if doplot:
-            mplot.doubleplot(MPLdat_masked,plotfilename=plotfilename,**plotkwargs)
-    else:
-        if docorrection:
-            MPLdat_event=mproc.basiccorrection(MPLdat_event,**corkwargs)
-            if docorplot:
-                mplot.doubleplot(MPLdat_event,plotfilename=coefplotfilename,**coefplotkwargs)
-        
-        if doplot:
-            mplot.doubleplot(MPLdat_event,plotfilename=plotfilename,**plotkwargs)
+    
+    if docorrection:
+        MPLdat_event=mproc.basiccorrection(MPLdat_event,**corkwargs)
+        if docorplot:
+            mplot.doubleplot(MPLdat_event,plotfilename=coefplotfilename,**coefplotkwargs)
+    
+    if doplot:
+        mplot.doubleplot(MPLdat_event,plotfilename=plotfilename,**plotkwargs)
             
-    if os.path.isdir(procsavepath):
-        os.chdir(procsavepath)
-    else:
-        os.makedirs(procsavepath)
-        os.chdir(procsavepath)
-    
-    if verbose:
-        print 'Saving '+d_filename
-    
-    if savetype=='standard':
-        MPLdat_event.save_to_HDF(d_filename)
-    elif savetype=='IDL':
-        MPLdat_event.save_to_IDL(d_filename)
+#    if os.path.isdir(procsavepath):
+#        os.chdir(procsavepath)
+#    else:
+#        os.makedirs(procsavepath)
+#        os.chdir(procsavepath)
+#    
+#    if verbose:
+#        print 'Saving '+d_filename
+#    
+#    if savetype=='standard':
+#        MPLdat_event.save_to_HDF(d_filename)
+#    elif savetype=='IDL':
+#        MPLdat_event.save_to_IDL(d_filename)
     
     if verbose:
         print 'Done'
@@ -229,7 +227,7 @@ def getmplfiles(newdir,interactive=True, verbose=False):
     return rawfiles
 
 def proccessall(**kwargs):
-    newdir=kwargs.get('newdir',[])
+    newdir=kwargs.get('newdir',None)
     chunksize=kwargs.get('chunksize','days')
     startdate=kwargs.get('startdate',datetime.datetime(2009,01,01))
     enddate=kwargs.get('enddate',datetime.datetime.now())
@@ -259,7 +257,7 @@ def proccessall(**kwargs):
     #findalllayers kwargs
     molthresh=kwargs.get('molthresh',1.0)
     layernoisethresh=kwargs.get('layernoisethresh',1.0)      
-    bg_alt=kwargs.get('bg_alt',[])
+    bg_alt=kwargs.get('bg_alt',None)
     datatype=kwargs.get('datatype','NRB')
     winsize=kwargs.get('winsize',5)
     wavelet=kwargs.get('wavelet',signal.ricker)
@@ -270,16 +268,16 @@ def proccessall(**kwargs):
     layerCWTrange=kwargs.get('layerCWTrange',np.arange(2,5))
     PBLwavelet=kwargs.get('PBLwavelet',mproc.dog)
     PBLCWTrange=kwargs.get('PBLCWTrange',np.arange(2,10))
-    sigma0=kwargs.get('sigma0',[])
+    sigma0=kwargs.get('sigma0',None)
     waterthresh=kwargs.get('waterthresh',0.10)
     icethresh=kwargs.get('icethresh',0.25)
     smokethresh=kwargs.get('smokethresh',0.05)
     dustthresh=kwargs.get('dustthresh',0.15)
     
     #correction kwargs
-    refalt=kwargs.get('refalt',[])
+    refalt=kwargs.get('refalt',None)
     method=kwargs.get('method','klett2')
-    lrat=kwargs.get('lrat',[])
+    lrat=kwargs.get('lrat',None)
     mode=kwargs.get('mode','copol')
     
     #doubleplot kwargs
@@ -365,7 +363,7 @@ def proccessall(**kwargs):
         
         
     
-def quickplot(filename, datadir = [], savefigs = False):
+def quickplot(filename, datadir = None, savefigs = False):
     
 #    MPLdat = mtools.MPL()
 #    
@@ -375,7 +373,7 @@ def quickplot(filename, datadir = [], savefigs = False):
     NRBdepol = pan.read_hdf(filename[0],'MPL')    
     
     if savefigs:    
-        if not datadir:
+        if datadir is None:
             datadir = mtools.set_dir("Select Folder for Figures to be depositied")
         
         os.chdir(datadir)
@@ -506,24 +504,24 @@ def clearall():
 if __name__ == '__main__':
     
     os.chdir('C:\Users\dashamstyr\Dropbox\Lidar Files\MPL Data\DATA\Ucluelet Files')
-    altrange=range(150,10180,30)
+    altrange=range(150,15030,30)
     timestep='240S'
     savetype='standard'
     procsavepath='.\Processed'
     plotsavepath='.\Figures'
     startdate=datetime.datetime(2014,05,03,00)
-    enddate=datetime.datetime(2014,05,03,23)
-    NRBmask=True
+    enddate=datetime.datetime(2014,05,03,15)
+    NRBmask=False
     NRBthresh=3.0
     NRBmin=0.5
     NRBminalt=150
     NRBnumprofs=1
     SNRmask=True
     SNRthresh=3.0
-    molthresh=0.5
+    molthresh=1.0
     layernoisethresh=1.0
     doplot=True
-    saveplot=True,
+    saveplot=False,
     showplot=True
     dolayers=True
     docorrection=True
@@ -532,8 +530,9 @@ if __name__ == '__main__':
     verbose=False
     NRB_limits=(0.0,1.0,0.2) 
     depol_limits=(0.0,0.5,0.1)
-    back_limits=(0.0,1e-7,2e-8)
+    back_limits=(0.0,2e-6,4e-7)
     ext_limits=(0.0,2e-5,4e-6)
+    interpolate='none'
     
     kwargs={'altrange':altrange,'timestep':timestep,'savetype':savetype,'procsavepath':procsavepath,
             'plotsavepath':plotsavepath,'startdate':startdate,'enddate':enddate,
@@ -541,9 +540,10 @@ if __name__ == '__main__':
             'doplot':doplot,'saveplot':saveplot,'showplot':showplot,'dolayers':dolayers,
             'docorrection':docorrection,'dolayerplot':dolayerplot,'docorplot':docorplot,
             'verbose':verbose,'NRBmask':NRBmask,'SNRmask':SNRmask,'NRB_limits':NRB_limits,
-            'depol_limits':depol_limits,'back_limits':back_limits,'ext_limits':ext_limits}
+            'depol_limits':depol_limits,'back_limits':back_limits,'ext_limits':ext_limits,
+            'interpolate':interpolate}
 
-    fileproc(**kwargs)
+    mpl1=fileproc(**kwargs)
 #    proccessall(**kwargs)
     
     

MPLhisttools.py
@@ -1,18 +1,22 @@
+from __future__ import absolute_import
+import os,sys,site
+home=os.environ['homepath']
+site.addsitedir('{0}\\Dropbox\\Python_Scripts\\GIT_Repos\\'.format(home))
+
 import numpy as np
-from numpy import ma
 from scipy.stats import cumfreq
-import glob, sys, os
 import time, datetime
 from math import isnan
 import pandas as pan
-import MPLtools as mtools
-import MPLprocesstools as mproc
-import MPLfileproc as mfile
-import MPLplot as mplot
 from matplotlib import pyplot as plt
 from matplotlib import cm, ticker
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import pickle
+
+from . import MPLtools as mtools
+from . import MPLprocesstools as mproc
+from . import MPLfileproc as mfile
+from . import MPLplot as mplot
 
 def progress(datanum,tot_loops):
     """
@@ -24,7 +28,7 @@ def progress(datanum,tot_loops):
     sys.stdout.write("\rpercent complete: {0}%".format(the_frac))
     sys.stdout.flush()
 
-def filegetter(filedir=[],filetype='.mpl',daterange=[],**kwargs):
+def filegetter(filedir=None,filetype='.mpl',daterange=None,**kwargs):
     """
     takes a top-level directory and finds all relevant files 
     returns a list of filenames to iterate through
@@ -33,14 +37,14 @@ def filegetter(filedir=[],filetype='.mpl',daterange=[],**kwargs):
     filedir = top-level directory.  If emplty, interactive window is used
     filetype = file extension to look for (.mpl or .h5), defaults to .mpl
     """    
-    altitudes = kwargs.get('altitudes',np.array([]))
+    altitudes = kwargs.get('altitudes',np.array(None))
     starttime = kwargs.get('starttime',datetime.datetime(1970,1,1,0))
     endtime = kwargs.get('endtime',datetime.datetime.now())
-    timestep = kwargs.get('timestep',[])
+    timestep = kwargs.get('timestep',None)
     verbose = kwargs.get('verbose',False)
     SNRmask=kwargs.get('SNRmask',True)
     SNRthreshold=kwargs.get('SNRthreshold',1)
-    bg_alt=kwargs.get('bg_alt',[])
+    bg_alt=kwargs.get('bg_alt',None)
     datatype=kwargs.get('datatype','data')
     NRBmask=kwargs.get('NRBmask',True)
     NRBthreshold=kwargs.get('NRBthreshold',3)
@@ -50,7 +54,7 @@ def filegetter(filedir=[],filetype='.mpl',daterange=[],**kwargs):
     topickle = kwargs.get('topickle',False)
     picklefile= kwargs.get('picklefile','test.p')
        
-    if not filedir:
+    if filedir is None:
         filedir = mtools.set_dir('Sepect top-level folder to search')
     
     outlist=[]
@@ -136,8 +140,8 @@ def dataextractor(datatype='NRB',**kwargs):
     
     """
     loadfiletype=kwargs.get('loadfiletype','list')
-    loadfilename=kwargs.get('loadfilename',[])
-    MPLlist=kwargs.get('MPLlist',[])
+    loadfilename=kwargs.get('loadfilename',None)
+    MPLlist=kwargs.get('MPLlist',None)
     toHDF=kwargs.get('toHDF',False)
     HDFfile=kwargs.get('HDFfile','test.h5')
     
@@ -204,7 +208,7 @@ def dataextractor(datatype='NRB',**kwargs):
 
 def scenefilter(dfin,panelin,**kwargs):
     filtertype=kwargs.get('filtertype',['aerosol'])
-    filtersubtype=kwargs.get('filtersubtype',[])
+    filtersubtype=kwargs.get('filtersubtype',None)
     verbose = kwargs.get('verbose',False)
     inplace=kwargs.get('inplace',True)
     
@@ -244,9 +248,9 @@ def histplot1D(df,**kwargs):
     fignum=kwargs.get('fignum',0)
     xlog=kwargs.get('xlog',False)
     ylog=kwargs.get('ylog',False)
-    xlimits=kwargs.get('xlimits',[])
-    ylimits=kwargs.get('ylimits',[])
-    xlabel=kwargs.get('xlabel',[])
+    xlimits=kwargs.get('xlimits',None)
+    ylimits=kwargs.get('ylimits',None)
+    xlabel=kwargs.get('xlabel',None)
     if ylog:
         ylabel=kwargs.get('ylabel','Log (Counts)')
     else:
@@ -316,8 +320,8 @@ def histplot2D(dfx,dfy,**kwargs):
     fsize=kwargs.get('fsize',32) #baseline font size
     ar=kwargs.get('ar',1.0)  #aspect ratio
     figheight=kwargs.get('figheight',12) #inches
-    xlabel=kwargs.get('xlabel',[])
-    ylabel=kwargs.get('ylabel',[])        
+    xlabel=kwargs.get('xlabel',None)
+    ylabel=kwargs.get('ylabel',None)        
     fignum=kwargs.get('fignum',0)
     cmap=kwargs.get('cmap',cm.bone)
     undercolor=kwargs.get('undercolor','r')
@@ -393,9 +397,9 @@ def althistplot(dfin,**kwargs):
     saveplot=kwargs.get('savefile',False)
     plotfilename=kwargs.get('plotfilename','2dalthist_test.png')
     fsize=kwargs.get('fsize',32) #baseline font size
-    ar=kwargs.get('ar',[])  #aspect ratio
+    ar=kwargs.get('ar',None)  #aspect ratio
     figheight=kwargs.get('figheight',12) #inches
-    xlabel=kwargs.get('xlabel',[])
+    xlabel=kwargs.get('xlabel',None)
     ylabel=kwargs.get('ylabel','Altitude [m]')        
     fignum=kwargs.get('fignum',0)
     cmap=kwargs.get('cmap',cm.bone)

MPLplot.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+import os,sys,site
+home=os.environ['homepath']
+site.addsitedir('{0}\\Dropbox\\Python_Scripts\\GIT_Repos\\'.format(home))
+
 import numpy as np
 import os, sys
 import numpy as np
@@ -6,10 +11,9 @@ import bisect
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import pandas as pan
-import MPLtools as mtools
-import MPLprocesstools as mproc
+from MPLcode import MPLtools as mtools
 from matplotlib.colors import LinearSegmentedColormap
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+#from mpl_toolkits.axes_grid1 import make_axes_locatable
 import operator
 from copy import deepcopy
 
@@ -55,9 +59,10 @@ def top_plot(ax, data, xdata, ydata, **kwargs):
     overcolor=kwargs.get('overcolor','w')
     undercolor=kwargs.get('undercolor','k')
     numvals=kwargs.get('numvals',50)
+    interpolation=kwargs.get('interpolation',None)
     
     my_cmap=custom_cmap(maptype=maptype,numvals=numvals,overcolor=overcolor,undercolor=undercolor)  
-    im = ax.imshow(data, vmin=vrange[0], vmax=vrange[1], cmap = my_cmap)
+    im = ax.imshow(data, vmin=vrange[0], vmax=vrange[1], cmap = my_cmap, interpolation=interpolation)
     forceAspect(ax,ar)       
     altticks(ax, ydata, fsize = fsize, tcolor = 'w')
     
@@ -82,9 +87,9 @@ def bottom_plot(fig,ax,data,xdata,ydata,**kwargs):
     overcolor=kwargs.get('overcolor','w')
     undercolor=kwargs.get('undercolor','k')
     numvals=kwargs.get('numvals',50)
-    
+    interpolation=kwargs.get('interpolation',None)
     my_cmap=custom_cmap(maptype=maptype,numvals=numvals,overcolor=overcolor,undercolor=undercolor)    
-    im = ax.imshow(data, vmin=vrange[0], vmax=vrange[1], cmap = my_cmap)
+    im = ax.imshow(data, vmin=vrange[0], vmax=vrange[1], cmap = my_cmap, interpolation=interpolation)
     forceAspect(ax,ar)        
     altticks(ax, ydata, fsize=fsize)  
     if orientation=='vertical':
@@ -103,7 +108,7 @@ def forceAspect(ax,aspect=1):
     extent =  im[0].get_extent()
     ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
 
-def dateticks(ax, axisdat,hours = [], fsize = 21, tcolor = 'k'):
+def dateticks(ax, axisdat,hours = None, fsize = 21, tcolor = 'k'):
     
     dold = axisdat[0].strftime('%d')
     hold = axisdat[0].strftime('%H')
@@ -119,7 +124,7 @@ def dateticks(ax, axisdat,hours = [], fsize = 21, tcolor = 'k'):
             tickmarks.append(n)
         else:
             htemp = d.strftime('%H')
-            if not hours:
+            if hours is None:
                 if htemp != hold:
                     ticklabels.append(d.strftime('%H'))
                     tickmarks.append(n)
@@ -270,10 +275,10 @@ def doubleplot(datafile,**kwargs):
     """
     
     #set kwarg defaults 
-    starttime = kwargs.get('starttime',[])
-    endtime = kwargs.get('endtime',[])
-    timestep = kwargs.get('timestep',[])
-    altrange = kwargs.get('altrange',[])
+    starttime = kwargs.get('starttime',None)
+    endtime = kwargs.get('endtime',None)
+    timestep = kwargs.get('timestep',None)
+    altrange = kwargs.get('altrange',None)
     hours = kwargs.get('hours',['03','06', '09','12', '15','18','21']) 
     fsize = kwargs.get('fsize',18) #baseline font size
     ar = kwargs.get('ar',2.0)  #aspect ratio
@@ -286,6 +291,7 @@ def doubleplot(datafile,**kwargs):
     orientation=kwargs.get('orientation','vertical')
     toptype=kwargs.get('toptype','NRB')
     bottomtype=kwargs.get('bottomtype','depol')
+    interpolation=kwargs.get('interpolation',None)
     
     if type(datafile)in [str,unicode]:
         savefilename = kwargs.get('savefilename','{0}.png'.format(datafile.split('.')[0]))
@@ -311,12 +317,12 @@ def doubleplot(datafile,**kwargs):
         
         MPLevent.fromHDF(datafile)    
     else:
-        MPLevent = datafile
+        MPLevent = deepcopy(datafile)
         
     if len(altrange)>0:
         MPLevent.alt_resample(altrange)    
     
-    if [i for i in [starttime,endtime,timestep] if len(i)>0]:
+    if any([starttime,endtime,timestep]):
         MPLevent.time_resample(timestep=timestep,starttime=starttime,endtime=endtime)
         
       
@@ -335,7 +341,7 @@ def doubleplot(datafile,**kwargs):
         if not MPLevent.depolrat:
             MPLevent.calculate_depolrat()
         if SNRmask:
-            MPLevent_masked=mproc.SNR_mask_depol(MPLevent,SNRthreshold=SNRthresh)
+            MPLevent_masked=mtools.SNR_mask_depol(MPLevent,SNRthreshold=SNRthresh)
             bottomdat = MPLevent_masked.depolrat[0]
         else:
             bottomdat = MPLevent.depolrat[0]
@@ -369,7 +375,7 @@ def doubleplot(datafile,**kwargs):
     ax1 = fig.add_subplot(2,1,1)
     im1 = top_plot(ax1,topdat.T[::-1],dtindex,alt[::-1],ar=ar, 
                            vrange=(topplot_min,topplot_max),fsize=fsize,maptype=colormap,
-                            orientation=orientation)
+                            orientation=orientation, interpolation=interpolation)
     cbar1 = fig.colorbar(im1, orientation = 'vertical', aspect = 6, extend='both')
     cbar1.set_ticks(np.arange(topplot_min,topplot_max+topplot_step,topplot_step))
     cbar1.set_ticklabels(np.arange(topplot_min,topplot_max+topplot_step,topplot_step))
@@ -383,7 +389,7 @@ def doubleplot(datafile,**kwargs):
     ax2 = fig.add_subplot(2,1,2)
     im2 = bottom_plot(fig, ax2, bottomdat.T[::-1],dtindex,alt[::-1],ar=ar, 
                      vrange=(bottomplot_min,bottomplot_max),fsize=fsize,maptype=colormap,
-                        orientation=orientation)
+                        orientation=orientation, interpolation=interpolation)
     cbar2 = fig.colorbar(im2, orientation = 'vertical', aspect = 6, extend='both')
     cbar2.set_ticks(np.arange(bottomplot_min,bottomplot_max+bottomplot_step,bottomplot_step))
     cbar2.set_ticklabels(np.arange(bottomplot_min,bottomplot_max+bottomplot_step,bottomplot_step))
@@ -444,66 +450,79 @@ def colormask_plot(mplin,**kwargs):
     #set color codes for different layers
     hours=kwargs.get('hours',['00','06','12','18'])
     fontsize=kwargs.get('fontsize',24)
-    cbar_ticklocs=kwargs.get('cbarticklocs',np.arange(0,9)+0.5)
-    altrange=kwargs.get('altrange',[])
-    datetimerange=kwargs.get('datetimerange',[])
+    altrange=kwargs.get('altrange',None)
+    datetimerange=kwargs.get('datetimerange',None)
     SNRmask=kwargs.get('SNRmask',False)
     SNRthresh=kwargs.get('SNRthresh',3.0)
     SNRtype=kwargs.get('SNRtype','NRB')
     saveplot=kwargs.get('saveplot',True)
     showplot=kwargs.get('showplot',True)
-    plotfilepath=kwargs.get('plotfilepath',[])
+    plotfilepath=kwargs.get('plotfilepath',None)
     plotfilename=kwargs.get('plotfilename','testmaskfig.png')
     dpi = kwargs.get('dpi',100)    
-    colordict=kwargs.get('colordict',{'molecular':0,
+    colordict=kwargs.get('colordict',{'Clear Air':0,
                                        'PBL':1,
-                                       'ice':2,
-                                       'water':3,
-                                       'mixed':4,
-                                       'dust':5,
-                                       'smoke':6,
-                                       'water_soluble':7,
-                                       'unidentified':8})
-
-    cmapdict =  {'red':    ((0.0, 176.0/255.0, 176.0/255.0),
-                            (0.1, 176.0/255.0, 255.0/255.0),
-                            (0.2, 255.0/255.0, 255.0/255.0),
-                            (0.33333, 255.0/255.0, 0.0/255.0),
-                            (0.44444, 0.0/255.0, 186.0/255.0),
-                            (0.55555, 186.0/255.0, 184.0/255.0),
-                            (0.66666, 184.0/255.0, 75.0/255.0),
-                            (0.77777, 75.0/255.0, 220.0/255.0),
-                            (0.88888, 220.0/255.0, 192.0/255.0),
-                            (1.0, 192.0/255.0, 192.0/255.0)),
+                                       'Ice Cloud':2,
+                                       'Water Cloud':3,
+                                       'Mixed Cloud':4,
+                                       'Dust-Rich Aerosol':5,
+                                       'Smoke-Rich Aerosol':6,
+                                       'Urban Aerosol':7,
+                                       'Unidentified Aerosol':8,
+                                       'Insufficient Signal':9})
+    
+    Clear=(176.0,244.0,230.0)
+    PBL=(255.0,69.0,0.0)
+    Ice=(255.0,255.0,255.0)
+    Water=(0.0,0.0,205.0)
+    Mixed=(186.0,85.0,211.0)
+    Dust=(184.0,134.0,11.0)
+    Smoke=(75.0,75.0,100.0)
+    Urban=(220.0,20.0,60.0)
+    Unidentified=(192.0,192.0,192.0)
+    Insufficient=(0.0,0.0,0.0)
+                                       
+    cmapdict =  {'red':    ((0.0, Clear[0]/255.0, Clear[0]/255.0),
+                            (0.1, Clear[0]/255.0, PBL[0]/255.0),
+                            (0.2, PBL[0]/255.0, Ice[0]/255.0),
+                            (0.3, Ice[0]/255.0, Water[0]/255.0),
+                            (0.4, Water[0]/255.0, Mixed[0]/255.0),
+                            (0.5, Mixed[0]/255.0, Dust[0]/255.0),
+                            (0.6, Dust[0]/255.0, Smoke[0]/255.0),
+                            (0.7, Smoke[0]/255.0, Urban[0]/255.0),
+                            (0.8, Urban[0]/255.0, Unidentified[0]/255.0),
+                            (0.9, Unidentified[0]/255.0, Insufficient[0]/255.0),
+                            (1.0, Insufficient[0]/255.0, Insufficient[0]/255.0)),
         
-                 'green':  ((0.0, 244.0/255.0, 244.0/255.0),
-                            (0.1, 244.0/255.0, 69.0/255.0),
-                            (0.2, 69.0/255.0, 255.0/255.0),
-                            (0.33333, 255.0/255.0, 0.0/255.0),
-                            (0.44444, 0.0/255.0, 85.0/255.0),
-                            (0.55555, 85.0/255.0, 134.0/255.0),
-                            (0.66666, 134.0/255.0, 75.0/255.0),
-                            (0.77777, 75.0/255.0, 20.0/255.0),
-                            (0.88888, 20.0/255.0, 192.0/255.0),
-                            (1.0, 192.0/255.0, 192.0/255.0)),
+                 'green':  ((0.0, Clear[1]/255.0, Clear[1]/255.0),
+                            (0.1, Clear[1]/255.0, PBL[1]/255.0),
+                            (0.2, PBL[1]/255.0, Ice[1]/255.0),
+                            (0.3, Ice[1]/255.0, Water[1]/255.0),
+                            (0.4, Water[1]/255.0, Mixed[1]/255.0),
+                            (0.5, Mixed[1]/255.0, Dust[1]/255.0),
+                            (0.6, Dust[1]/255.0, Smoke[1]/255.0),
+                            (0.7, Smoke[1]/255.0, Urban[1]/255.0),
+                            (0.8, Urban[1]/255.0, Unidentified[1]/255.0),
+                            (0.9, Unidentified[1]/255.0, Insufficient[1]/255.0),
+                            (1.0, Insufficient[1]/255.0, Insufficient[1]/255.0)),
         
-                 'blue':   ((0.0, 230.0/255.0, 230.0/255.0),
-                            (0.1, 230.0/255.0, 0.0/255.0),
-                            (0.2, 0.0/255.0, 255.0/255.0),
-                            (0.33333, 255.0/255.0, 205.0/255.0),
-                            (0.44444, 205.0/255.0, 211.0/255.0),
-                            (0.55555, 211.0/255.0, 11.0/255.0),
-                            (0.66666, 11.0/255.0, 100.0/255.0),
-                            (0.77777, 100.0/255.0, 60.0/255.0),
-                            (0.88888, 60.0/255.0, 192.0/255.0),
-                            (1.0, 192.0/255.0, 192.0/255.0))}    
+                 'blue':   ((0.0, Clear[2]/255.0, Clear[2]/255.0),
+                            (0.1, Clear[2]/255.0, PBL[2]/255.0),
+                            (0.2, PBL[2]/255.0, Ice[2]/255.0),
+                            (0.3, Ice[2]/255.0, Water[2]/255.0),
+                            (0.4, Water[2]/255.0, Mixed[2]/255.0),
+                            (0.5, Mixed[2]/255.0, Dust[2]/255.0),
+                            (0.6, Dust[2]/255.0, Smoke[2]/255.0),
+                            (0.7, Smoke[2]/255.0, Urban[2]/255.0),
+                            (0.8, Urban[2]/255.0, Unidentified[2]/255.0),
+                            (0.9, Unidentified[2]/255.0, Insufficient[2]/255.0),
+                            (1.0, Insufficient[2]/255.0, Insufficient[2]/255.0))}    
                    
     maskmap=LinearSegmentedColormap('MaskMap',cmapdict)
-    maskmap.set_under(color='k')
     
     
     if SNRmask:
-        mplmasked=mproc.SNR_mask_colors(mplin,SNRthresh=SNRthresh,datatype=SNRtype,inplace=False)
+        mplmasked=mtools.SNR_mask_colors(mplin,SNRthresh=SNRthresh,datatype=SNRtype,inplace=False)
         maskin=mplmasked.scenepanel[0]['colormask']
     else:
         maskin=deepcopy(mplin.scenepanel[0]['colormask'])
@@ -533,6 +552,7 @@ def colormask_plot(mplin,**kwargs):
 #    cax = divider.append_axes("bottom", size="10%", pad=0.15)
     cbar1=fig.colorbar(image,cax=cax,orientation='horizontal')
 #    cbar1.ax.set_autoscalex_on(False)
+    cbar_ticklocs=np.arange(len(colordict)+0.5)
     cbar1.set_ticks(cbar_ticklocs)
     cbar1.ax.tick_params(bottom='off',top='off',labelsize=fontsize-4)
     
@@ -590,12 +610,12 @@ if __name__=='__main__':
     
     MPLevent.time_resample(timestep,starttime,endtime)  
     MPLevent.calc_all()
-    MPLevent=mproc.NRB_mask_all(MPLevent) 
+    MPLevent=mtools.NRB_mask_all(MPLevent) 
     
     kwargs = {'saveplot':True,'showplot':True,'verbose':True,
                 'savefilepath':savepath,'savefilename':savefilename,
                 'hours':hours,'bottomplot_limits':bottomplot_limits,'topplot_limits':topplot_limits,'SNRmask':True,
-                'colormap':'customjet','orientation':'vertical'}
+                'colormap':'customjet','orientation':'vertical','interpolation':none}
     
     doubleplot(MPLevent,**kwargs)
     

inversiontools.py
@@ -3,6 +3,8 @@ import pandas as pan
 from copy import deepcopy
 import random
 import matplotlib.pyplot as plt
+from itertools import groupby
+import operator
 
 def std_atm(z):
     ########################################################################
@@ -213,7 +215,7 @@ def calcprof(betaprof,sigmaprof,E0=1.0,C = 1.0,T_0=1.0):
     
     return P_z
 
-def addlayer(P_in, beta, lrat, bottom=[], top=[], inplace=True):
+def addlayer(P_in, beta, lrat, bottom=None, top=None, inplace=True):
     """
     Function that adds a layer of known backscatter coefficient
     and lidar ratio onto an existing lidar response profile
@@ -236,9 +238,9 @@ def addlayer(P_in, beta, lrat, bottom=[], top=[], inplace=True):
     if type(beta)!=float:
         z_in = np.array(beta.index.values, dtype='float64')
     else:
-        try:
+        if bottom is not None and top is not None:
             z_in = np.array(P_out.index[(P_out.index>=bottom)&(P_out.index<=top)],dtype='float64')
-        except IndexError:
+        else:
             print "You must define a layer top and bottom"
             return
             
@@ -305,7 +307,7 @@ def backandnoise(P_in,background = 0.0,stdev = 0.0,inplace=True):
        
     return P_out
 
-def background_subtract(P_in,back_avg=[],z_min=[],inplace=True):
+def background_subtract(P_in,back_avg=None,z_min=None,inplace=True):
     #subtracts background from signal, without background
     #takes advantage of inverse square law to calculate background signal for
     #a profile
@@ -318,12 +320,12 @@ def background_subtract(P_in,back_avg=[],z_min=[],inplace=True):
     else:
         P_out = deepcopy(P_in)
     
-    if back_avg:
+    if back_avg is not None:
         P_out['vals']=P_out['vals']-back_avg
     else:    
         #select data from altitudes higher than z_min and muliply full signal by
         #range squared, if this gives less than 500 values, take uppermost 500
-        if z_min:
+        if z_min is not None:
             z=P_out.index[P_out.index>=z_min]
         else:
             z=P_out.index[-100:]
@@ -339,7 +341,7 @@ def background_subtract(P_in,back_avg=[],z_min=[],inplace=True):
         
     return P_out
 
-def calcNRB(P_in,E0=1.0,background=[],inplace=True):
+def calcNRB(P_in,E0=1.0,background=None,inplace=True):
     P_out=background_subtract(P_in,back_avg=background,inplace=inplace)
     P_out['rsq'] = P_out['vals']*(P_in.index.values/1000.0)**2.0
     P_out['NRB'] = P_out['rsq']/E0
@@ -429,7 +431,7 @@ def profgen(z,**kwargs):
     wave = kwargs.get('wave',532.0)  #nm
     E0 = kwargs.get('E0',1.0)
     C = kwargs.get('C',1.0)
-    layers = kwargs.get('layers',[])
+    layers = kwargs.get('layers',None)
     background = kwargs.get('background',0.0) #background signal level
     noise = kwargs.get('noise',0.0) #use defined noise level, defaults to 0
     donorm = kwargs.get('donorm',False) #if true, profile is normalized by first value
@@ -439,7 +441,7 @@ def profgen(z,**kwargs):
     
     #add particulate layers, if any
     
-    if layers:
+    if layers is not None:
         for n in range(len(layers)):
             templayer=layers[n]
             if type(templayer['beta_p'])==float:
@@ -460,7 +462,7 @@ def profgen(z,**kwargs):
     return P_out
     
 
-def fernald(P_in, lrat, wave = 532.0, E = 1.0, calrange = []):
+def fernald(P_in, lrat, wave = 532.0, E = 1.0, calrange = None):
     """
     Inputs:
         P_in: a pandas series depicting a 1-D profile of NRB from an MPL class object. 
@@ -507,7 +509,7 @@ def fernald(P_in, lrat, wave = 532.0, E = 1.0, calrange = []):
     beta_total = pan.Series(index=altitudes)
     
         
-    if not calrange:
+    if calrange is None:
         for alt in reversed(altitudes):
             if alt == altitudes[-1]:
                 #at calibration altitude assume no aerosols
@@ -587,7 +589,7 @@ def fernald(P_in, lrat, wave = 532.0, E = 1.0, calrange = []):
     return beta_total,sigma_total
 
 
-def klett(P_in,lrat_in,r_m,sigma_m,k=1):
+def klett(P_in,lrat_in,r_m=None,k=1,wave=532.0):
     """
     Function that calculates backscatter and extinction coefficients based on
     variable lidar ratios using the Klett algorithm 
@@ -606,11 +608,17 @@ def klett(P_in,lrat_in,r_m,sigma_m,k=1):
     sigma = pandas series of extinction coefficients
     
     """
+
     
-    S=np.log(P_in)
+    S=np.log(P_in).fillna(method='pad',inplace=True)
     lrat = 1.0/lrat_in  #Klett definition of lidar ratio is backscatter/extintion not the other way round
 
     altitudes = S.index.values
+    if r_m is None:
+        for a in altitudes[::-1]:
+            if not np.isnan(P_in.ix[a]):
+                r_m=a
+                break
     
     sigma = pan.Series(index=altitudes,dtype='float')
     beta = pan.Series(index=altitudes,dtype='float')
@@ -624,7 +632,7 @@ def klett(P_in,lrat_in,r_m,sigma_m,k=1):
            break
     
     newalts = S_new.index.values
-    
+    sigma_m=molprof(z=newalts,wave=wave)['sigma_R'].loc[r_m]
     for alt in reversed(newalts):
         if alt == newalts[-1]:            
             sigma.loc[alt] = sigma_m
@@ -644,7 +652,8 @@ def klett(P_in,lrat_in,r_m,sigma_m,k=1):
 def klett2(P_in,lrat_in,**kwargs):
     """
     Function that calculates backscatter and extinction coefficients based on
-    variable lidar ratios using the Klett algorithm 
+    variable lidar ratios using the Klett algorithm .  Note: requires lrat_in not
+    contain zero values except for at r_m altitude
     
     Inputs:
     P_in = a pandas series with values proportional to range squared corrected signal strength and altitude index
@@ -658,15 +667,14 @@ def klett2(P_in,lrat_in,**kwargs):
     sigma = pandas series of extinction coefficients
     
     """
-    r_m=kwargs.get('r_m',[])
-    beta_m=kwargs.get('beta_m',[])
+    r_m=kwargs.get('r_m',None)
     wave=kwargs.get('wave',532.0)
     
-    lrat = 1.0/lrat_in  #Klett definition of lidar ratio is backscatter/extintion not the other way round
+    lrat = (1.0/lrat_in).replace(np.inf,0.0) #Klett definition of lidar ratio is backscatter/extintion not the other way round
     lrat_R=3.0/(8.0*np.pi)
     altitudes = P_in.index.values
     
-    if not r_m:
+    if r_m is None:
         for a in altitudes[::-1]:
             if not np.isnan(P_in.ix[a]):
                 r_m=a
@@ -683,65 +691,98 @@ def klett2(P_in,lrat_in,**kwargs):
     newalts = P_new.index.values
     P_mol=molprof(z=newalts,wave=wave)
     beta_R=P_mol['beta_R']
-    if not beta_m:
-        beta_m=beta_R.loc[r_m]
+    beta_m=beta_R.loc[r_m]
     
-    sigma_m=beta_m/lrat_R
-    S=np.log(P_new)
-    S.fillna(method='pad',inplace=True)
+    sigma_R=P_mol['sigma_R']
+    sigma_m=sigma_R.loc[r_m]
+    #add arbitrary translation before transformation to avoid negative numbers in log value
+    #because inversion is based on the differential of dS/dr, this changes nothing in solution    
+    P_trans = P_new+0.001-np.min(P_new.values)
+    S=np.log(P_trans)
     
     for alt in reversed(newalts):
-        if alt == newalts[-1]: 
-            try:
-                beta.ix[alt]=beta_m
-            except ValueError:
-                print alt
+        if alt == r_m: 
+            beta.ix[alt]=beta_m
             sigma.ix[alt]=sigma_m
             S_m=S.ix[alt]
             beta_int1=0
             beta_int2=0
             S_int=0
-            S_R_int=0
-            oldalt = alt
-        
+            oldalt = alt      
         else:
-            delta_r=oldalt-alt
-            if lrat.ix[alt]==np.inf:
-                if lrat.ix[oldalt]!=np.inf:
-                    S_R_int=0
-                    beta.ix[alt]=beta_R.ix[alt]
-                    sigma.ix[alt]=beta_R.ix[alt]/lrat_R
-                    sigma_m=sigma.ix[alt]
-                    S_m=S.ix[alt]
-                    oldalt=alt
-                else:                    
-                    S_R_int+=0.5*(np.exp(S.ix[alt]-S_m)+np.exp(S.ix[oldalt]-S_m))*delta_r
-                    sigma.ix[alt]=np.exp(S.ix[alt]-S_m)/(sigma_m**-1+2.0*S_R_int)
-                    beta.ix[alt]=sigma.ix[alt]*lrat_R
-                    oldalt = alt
+            delta_r=oldalt-alt  
+            if oldalt==r_m:
+                #in this case, use simple reimann integration
+                beta_int1 += (2.0/lrat_R)*(beta_R.ix[alt])*delta_r
+                beta_int2 += 2.0*(beta_R.ix[alt]/lrat.ix[alt])*delta_r
+                delta_Sprime = S.ix[alt]-S_m+beta_int1-beta_int2
+                S_int+=2.0*(np.exp(delta_Sprime)/lrat.ix[alt])*delta_r
             else:
-                if lrat.ix[oldalt]==np.inf:
-                    beta_int1=0
-                    beta_int2=0
-                    S_int=0
-                    beta_m=beta.ix[oldalt]
-                    S_m=S.ix[oldalt]
-                    
-                beta_int1+=0.5*((2.0/lrat_R)*(beta_R.ix[alt]+beta_R.ix[oldalt]))*delta_r
+                #otherwise use trapezoidal integration
+                beta_int1+=0.5*((2.0/lrat_R)*(beta_R.ix[alt]+beta_R.ix[oldalt]))*delta_r   
                 beta_int2+=0.5*(2.0*((beta_R.ix[alt]/lrat.ix[alt])+(beta_R.ix[oldalt]/lrat.ix[oldalt])))*delta_r            
-                delta_Sprime=S.ix[alt]-S_m+beta_int1-beta_int2
-                S_int+=0.5*(2.0*((np.exp(delta_Sprime)/lrat.ix[alt])+(np.exp(delta_Sprime)/lrat.ix[oldalt])))*delta_r
+                delta_Sprime=S.ix[alt]-S_m+beta_int1-beta_int2                
+                S_int+=0.5*(2.0*((np.exp(delta_Sprime)/lrat.ix[alt])+(np.exp(old_delta_Sprime)/lrat.ix[oldalt])))*delta_r
                 
-                beta.ix[alt]=np.exp(delta_Sprime)/((1.0/beta_m)+S_int)
+            beta.ix[alt]=np.exp(delta_Sprime)/((1.0/beta_m)+S_int)
                 
-                if beta.ix[alt]<=beta_R.ix[alt]:
-                    beta_p=0.0
-                else:
-                    beta_p=beta.ix[alt]-beta_R.ix[alt]
-                sigma.ix[alt]=(beta_p/lrat.ix[alt])+beta_R.ix[alt]/lrat_R
-                oldalt = alt
-                
+            if beta.ix[alt]<=beta_R.ix[alt]:
+                beta_p=0.0
+            else:
+                beta_p=beta.ix[alt]-beta_R.ix[alt]
+            sigma.ix[alt]=(beta_p/lrat.ix[alt])+beta_R.ix[alt]/lrat_R
+            oldalt = alt
+            old_delta_Sprime=delta_Sprime
     return beta,sigma
+
+def invert_profile(profin,lratin,**kwargs):
+    method=kwargs.get('method','klett2')
+    refalt=kwargs.get('refalt',profin.index[-1])
+    backscatter=pan.Series(np.nan,index=profin.index)
+    extinction=pan.Series(np.nan,index=profin.index)
+    
+    mollayers=lratin[lratin==0.0]
+    moledges=[]
+    altstep=lratin.index[1]-lratin.index[0]
+    
+    for key,alt in groupby(enumerate(mollayers.index),lambda (i,x):i-(x-mollayers.index[0])/altstep):
+        temprange=map(operator.itemgetter(1),alt)
+        moledges.append((temprange[0],temprange[-1]))
+    
+    alt=profin.index[-1]
+    tempedges=moledges.pop()
+    while True:        
+        if tempedges[0]<alt<=tempedges[1]: 
+            layeralts=profin.ix[tempedges[0]:tempedges[1]].index.values
+            tempmol=molprof(z=layeralts)
+            backscatter.ix[layeralts]=tempmol['beta_R'].values
+            extinction.ix[layeralts]=tempmol['sigma_R'].values
+            alt=layeralts[0]
+        else:
+            try:
+                tempedges=moledges.pop()
+                layeralts=profin.ix[tempedges[1]:alt].index[1:]
+                alt=tempedges[1]
+            except IndexError:
+                layeralts=profin.ix[:alt].index
+                alt=layeralts[0]
+                
+            layerprof=profin.ix[layeralts]
+            layerlrat=lratin.ix[layeralts]
+
+            if method=='klett2':
+                tempback,tempext=klett2(layerprof,layerlrat,r_m=layeralts[-1])
+    
+            backscatter.ix[layeralts]=tempback.values
+            extinction.ix[layeralts]=tempext.values
+            
+        if alt==profin.index[0]:
+            break
+        
+    return backscatter,extinction
+            
+    #Step 2: Use Fernald algorithm for molecular sections    
+    #Step 3: Use klett2 for layers
 
 def lrat_tester_full(P_0,**kwargs):
     wave=kwargs.get('wave',532.0)
@@ -749,7 +790,7 @@ def lrat_tester_full(P_0,**kwargs):
     method=kwargs.get('method','klett2')
     lrat_klett=kwargs.get('lrat_klett',np.arange(.50,1.55,.5))
     lrat_fern=kwargs.get('lrat_fern',np.arange(15,80))
-    calrange_fern=kwargs.get('calrange_fern',[])
+    calrange_fern=kwargs.get('calrange_fern',None)
     r_m=kwargs.get('r_m',[])
     k=kwargs.get('k',1.0)
     
@@ -770,13 +811,11 @@ def lrat_tester_full(P_0,**kwargs):
         lratprof=P_0['sigma_t']/P_0['beta_t'] 
         lratprof.fillna(value=8.0*np.pi/3.0,inplace=True)
         if not r_m:
-            r_m=P_0.index.values[-1]
-        
-        sigma_m=P_0['sigma_t'].loc[r_m]        
+            r_m=P_0.index.values[-1]    
         for tempdelt in lrat_klett:
             lrattemp=lratprof*tempdelt
             beta_out[tempdelt],sigma_out[tempdelt]=klett(P_in=P_0['NRB'],lrat_in=lrattemp,
-                                                            r_m=r_m,sigma_m=sigma_m,k=k)
+                                                            r_m=r_m,k=k,wave=wave)
             deltabeta[tempdelt]=100.0*(P_0['beta_t']-beta_out[tempdelt])/P_0['beta_t']
             deltasigma[tempdelt]=100.0*(P_0['sigma_t']-sigma_out[tempdelt])/P_0['sigma_t']
     elif method=='klett2':
@@ -785,15 +824,13 @@ def lrat_tester_full(P_0,**kwargs):
         deltabeta=pan.DataFrame(index=P_0.index,columns=lrat_klett)
         deltasigma=pan.DataFrame(index=P_0.index,columns=lrat_klett)
         lratprof=P_0['sigma_p']/P_0['beta_p']         
-        lratprof.fillna(value=0,inplace=True)
+        lratprof.fillna(value=0.0,inplace=True)
         if not r_m:
             r_m=P_0.index.values[-1]
-        
-        beta_m=P_0['beta_t'].loc[r_m] 
+
         for tempdelt in lrat_klett:
             lrattemp=lratprof*tempdelt
-            beta_out[tempdelt],sigma_out[tempdelt]=klett2(P_in=P_0['NRB'],lrat_in=lrattemp,
-                                                            r_m=r_m,beta_m=beta_m)
+            beta_out[tempdelt],sigma_out[tempdelt]=klett2(P_in=P_0['NRB'],lrat_in=lrattemp,r_m=r_m)
             deltabeta[tempdelt]=100.0*(P_0['beta_t']-beta_out[tempdelt])/P_0['beta_t']
             deltasigma[tempdelt]=100.0*(P_0['sigma_t']-sigma_out[tempdelt])/P_0['sigma_t']
     
@@ -808,8 +845,8 @@ def lrat_tester_quick(P_0,**kwargs):
     method=kwargs.get('method','klett2')
     lrat_klett=kwargs.get('lrat_klett',np.arange(-.50,.55,.5))
     lrat_fern=kwargs.get('lrat_fern',np.arange(15,80))
-    calrange_fern=kwargs.get('calrange_fern',[])
-    r_m=kwargs.get('r_m',[])
+    calrange_fern=kwargs.get('calrange_fern',None)
+    r_m=kwargs.get('r_m',None)
     k=kwargs.get('k',1.0)
     lrat_type=kwargs.get('lrat_type','part')
     
@@ -910,7 +947,7 @@ if __name__ == '__main__':
     layer1={'beta_p':beta_layer,'lrat':15}
     P_0=profgen(z,layers=[layer1],background=background,noise=noise)
 
-    testpan=lrat_tester_full(P_0,lrat_klett=[0.5,1.0,1.5])
+    testpan=lrat_tester_full(P_0,lrat_klett=[1.0])
                 
 #    beta_fern = fernald(P_1,30,wave,1.0)
 #    sigma_fern=beta_fern*lrat_p1