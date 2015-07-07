Fixed bug in inversiontools where division by zero was occurring, corrected bug caused by new verson of imshow requring float dtype, converted all altitude units to km and backscatter/extinction to per km

MPLfileproc.py
@@ -505,42 +505,46 @@ def clearall():
         
 if __name__ == '__main__':
     
-    os.chdir('C:\Users\dashamstyr\Dropbox\Lidar Files\MPL Data\DATA\Ucluelet Files')
-    altrange=range(150,15030,30)
+#    os.chdir('C:\Users\dashamstyr\Dropbox\Lidar Files\MPL Data\DATA\Ucluelet Files')
+    os.chdir('K:\MPL Backup 20150706')
+    altrange=np.arange(0.150,5.030,0.030)
     timestep='120S'
     savetype='standard'
     procsavepath='.\Processed'
     plotsavepath='.\Figures'
-    startdate=datetime.datetime(2014,05,03,00)
-    enddate=datetime.datetime(2014,05,03,15)
+    startdate=datetime.datetime(2011,05,03,00)
+    enddate=datetime.datetime(2041,05,03,15)
+    
     NRBmask=False
     NRBthresh=3.0
     NRBmin=0.5
-    NRBminalt=150
+    NRBminalt=0.150
     NRBnumprofs=1
     SNRmask=False
-    SNRthresh=3.0
+    SNRthresh=1.0
+    
     molthresh=1.0
     layernoisethresh=1.0
-    sigma0=0.4
+    sigma0=0.1
     cloudthresh=(1.0,0.20)
-    waterthresh=0.10
+    waterthresh=0.05
     icethresh=0.25
     smokethresh=0.05
-    dustthresh=0.15
+    dustthresh=0.20
  
-    doplot=False
-    saveplot=False,
+    doplot=True
+    saveplot=True
     showplot=True
     dolayers=True
-    docorrection=False
+    docorrection=True
     dolayerplot=True
-    docorplot=False
+    docorplot=True
     verbose=False
-    NRB_limits=(0.0,1.0,0.2) 
+    hours=['03','06','09','12','15','18','21']
+    NRB_limits=(0.0,0.2,0.05) 
     depol_limits=(0.0,0.5,0.1)
-    back_limits=(0.0,2e-3,4e-4)
-    ext_limits=(0.0,2e-2,4e-3)
+    back_limits=(0.0,0.05,0.01)
+    ext_limits=(0.0,2.0,0.5)
     interpolate='none'
     
     kwargs={'altrange':altrange,'timestep':timestep,'savetype':savetype,'procsavepath':procsavepath,
@@ -552,7 +556,7 @@ if __name__ == '__main__':
             'depol_limits':depol_limits,'back_limits':back_limits,'ext_limits':ext_limits,
             'interpolate':interpolate, 'sigma0':sigma0, 'cloudthresh':cloudthresh,
             'waterthresh':waterthresh,'icethresh':icethresh,'smokethresh':smokethresh,
-            'dustthresh':dustthresh}
+            'dustthresh':dustthresh,'hours':hours}
 
     mpl1=fileproc(**kwargs)
 #    proccessall(**kwargs)

MPLprocesstools.py
@@ -59,7 +59,7 @@ def molecular_detect(MPLin,**kwargs):
             tempratvals=bufferedprof[n:n+winsize]
             coef.iloc[n]=np.mean(tempratvals)
         #Step 4: Result from 3 is profile of multiplying factor.Use this to calculate variance of residuals
-        rawvariance=((1/(z/1000.0)**2.0)*(tempprof-(Pmol_cor/coef)))**2.0
+        rawvariance=(1/(z**2.0)*(tempprof-(Pmol_cor/coef)))**2.0
         bufferedvariance=mtools.buffered_array(rawvariance.values,(1,winsize))
         variance=pan.Series(index=rawvariance.index)
         for n in range(len(rawvariance.values)):
@@ -310,14 +310,12 @@ def find_layers(MPLin,**kwargs):
     #use raw data from co-polarized channel (not r-squared corrected) to find layers
     if datatype=='data':
         rawdata=MPLin.data[0]
-        rawdepol=MPLin.data[1]
     elif datatype=='rsq':
         rawdata=MPLin.rsq[0]
-        rawdepol=MPLin.rsq[1]
     elif datatype=='NRB':
         rawdata=MPLin.NRB[0]
-        rawdepol=MPLin.NRB[1]
-    
+
+    rawdepol=MPLin.depolrat[0]
     rawsigma=MPLin.sigma[datatype][0]
     depolsigma=MPLin.sigma['depolrat'][0]  
     panelout=pan.Panel(major_axis=rawdata.index,minor_axis=['Base','Peak','Top',
@@ -691,6 +689,7 @@ def findalllayers(**kwargs):
     savemasks=kwargs.get('savemasks',False)
     savemaskname=kwargs.get('savemaskname','testmasksall.h5')
     sigma0=kwargs.get('sigma0',None)
+    depolsigma0=kwargs.get('depolsigma0',None)
     waterthresh=kwargs.get('waterthresh',0.10)
     icethresh=kwargs.get('icethresh',0.25)
     smokethresh=kwargs.get('smokethresh',0.05)
@@ -709,7 +708,7 @@ def findalllayers(**kwargs):
                    'cloudthresh':cloudthresh,'datatype':datatype,'CWTwidth':CWTwidth,
                    'widths':layerCWTrange,'minwidth':minwidth,'bg_alt':bg_alt,
                    'waterthresh':waterthresh,'icethresh':icethresh,'smokethresh':smokethresh,
-                   'dustthresh':dustthresh,'sigma0':sigma0}
+                   'dustthresh':dustthresh,'sigma0':sigma0,'depolsigma0':depolsigma0}
     layers=find_layers(mplin,**layerkwargs)  
     mol_min=molecular.loc['Layer0']
     try:
@@ -786,7 +785,7 @@ def scenemaker(layerdict,**kwargs):
     molrat=kwargs.get('molrat',0.0)
     moldepol=kwargs.get('moldepol',0.0035)
     udefrat=kwargs.get('udefrat',molrat)
-    udefdepol=kwargs.get('udefdepol',0.0035)
+    udefdepol=kwargs.get('udefdepol',moldepol)
     savefile=kwargs.get('savefile',False)
     savefilename=kwargs.get('savefilename','test.h5')
     colordict=kwargs.get('colordict',None)
@@ -1339,18 +1338,20 @@ if __name__=='__main__':
     savefilename='{0}_scenepanel.h5'.format(filename.split('_proc')[0])
     plotfilename='{0}_coefplot.png'.format(filename.split('_proc')[0])
     timestep='240S'
-    altrange = np.arange(150,15030,30)
+    altrange = np.arange(0.150,15.030,0.030)
     SNRthreshold=1.0
-    molthresh=0.01
+    molthresh=1.0
     layernoisethresh=1.0
-    sigma0=None
+    sigma0=0.1
+    depolsigma0=0.05
     mpltest=mtools.MPL()
     mpltest.fromMPL(filepath)
     mpltest.alt_resample(altrange)
     mpltest.calc_all()
     
     layerdict=findalllayers(mplin=mpltest,timestep=timestep,molthresh=molthresh,
-                            layernoisethresh=layernoisethresh,datatype='NRB',sigma0=sigma0)
+                            layernoisethresh=layernoisethresh,datatype='NRB',sigma0=sigma0,
+                            depolsigma0=depolsigma0)
     mpltest=scenemaker(layerdict)
     mpltest=basiccorrection(mpltest,inplace=True)
     #    mpltemp.save_to_HDF(savefilename)

MPLtools.py
@@ -201,7 +201,7 @@ class MPL:
             
                     headerdat['bintime'] = floatarray[2]  #bin width in seconds
                     
-                    headerdat['rangecal'] = floatarray[3] #range offset in meters, default is 0
+                    headerdat['rangecal'] = floatarray[3]/1000.0 #range offset in km, default is 0
             
                     headerdat['databins'] = intarray16[9]  #number of bins not including those used for background
                     headerdat['scanflag'] = intarray16[10]  #0: no scanner, 1: scanner
@@ -253,7 +253,7 @@ class MPL:
 
                     numbins = headerdat['numbins']
                     numchans = headerdat['numchans'] 
-                    altstep = headerdat['bintime']*const.c/2 #altitude step in meters
+                    altstep = headerdat['bintime']*const.c/2000.0 #altitude step in km
                     maxalt = numbins*altstep
                     firstbin=headerdat['firstbin']
                     minalt = headerdat['rangecal']+firstbin*altstep
@@ -277,12 +277,12 @@ class MPL:
         df_copol = pan.DataFrame.from_dict(profdat_copol,orient = 'index')
         df_copol.columns = altrange
         #minimum usable altitude is 150m for overlap reasons
-        if altrange[0]<=150.0:
-            df_copol=df_copol.loc[:,150.0:]
+        if altrange[0]<=0.150:
+            df_copol=df_copol.loc[:,0.150:]
         df_crosspol = pan.DataFrame.from_dict(profdat_crosspol,orient = 'index')
         df_crosspol.columns = altrange
-        if altrange[0]<=150.0:
-            df_crosspol=df_crosspol.loc[:,150.0:]
+        if altrange[0]<=0.150:
+            df_crosspol=df_crosspol.loc[:,0.150:]
         df_header = pan.DataFrame.from_dict(header, orient = 'index')
                 
         self.data = [df_copol, df_crosspol]
@@ -850,7 +850,7 @@ class MPL:
         bg = [self.header['bg_avg2'],self.header['bg_avg1']]
       
         for n in range(self.header['numchans'][0]): 
-            rsq = (np.array(dataout[n].columns, dtype=float)/1000.0)**2
+            rsq = (np.array(dataout[n].columns, dtype=float))**2
             for i in self.data[n].index:
                 dataout[n].ix[i] = (self.data[n].ix[i] - bg[n].ix[i])*rsq
         
@@ -924,7 +924,7 @@ class MPL:
                 
             numpairs = (numvals-1)/2
             mean_energy = np.array(afterpulsedat[0])
-            aprange = np.array(afterpulsedat[1:numpairs+1])*1000.0
+            aprange = np.array(afterpulsedat[1:numpairs+1])
             apvals_copol = np.array(afterpulsedat[numpairs+1:])/mean_energy
             apvals_crosspol=apvals_copol
             apvals=[apvals_copol,apvals_crosspol]
@@ -938,7 +938,7 @@ class MPL:
                 overlapdat.fromfile(binfile,numvals)
             
             numpairs = numvals/2
-            overrange = np.array(overlapdat[:numpairs])*1000.0
+            overrange = np.array(overlapdat[:numpairs])
             overvals = np.array(overlapdat[numpairs:])    
             
             altvals = np.array(tempdat[0].columns, dtype='float')
@@ -995,7 +995,6 @@ class MPL:
                     apbg_crosspol = struct.unpack('d',temp)[0]
                     
                     aprange.fromfile(binfile,apnumbins)
-                    aprange = [n*1000.0 for n in aprange] # convert range values to meters
                     apvals_copol.fromfile(binfile,apnumbins)
                     copol_norm = [n/apenergy for n in apvals_copol] #convert to counts/us
                     apvals_crosspol.fromfile(binfile,apnumbins)
@@ -1015,7 +1014,7 @@ class MPL:
                 overlapdat.fromfile(binfile,numvals)
                 
             numpairs = numvals/2
-            overrange = np.array(overlapdat[:numpairs])*1000.0
+            overrange = np.array(overlapdat[:numpairs])
             overvals = np.array(overlapdat[numpairs:])    
         
             interp_overlap = np.interp(altvals,overrange,overvals)
@@ -1033,7 +1032,7 @@ class MPL:
         for n in range(numchans):
             interp_afterpulse = np.interp(altvals,aprange,apvals[n])
             NRBtemp=pan.DataFrame(index=tempdat[n].index,columns=tempdat[n].columns)
-            rsq = (np.array(tempdat[n].columns, dtype=float)/1000.0)**2
+            rsq = (np.array(tempdat[n].columns, dtype=float))**2
             for i in range(len(tempdat[n].index)):
                 tempval = ((tempdat[n].iloc[i] - bg[n][i])*deadtimecor[n,i] - interp_afterpulse)/energy[i]                
                 NRBtemp.iloc[i] = tempval*rsq/interp_overlap
@@ -1244,7 +1243,7 @@ class MPL:
                         SNRprof=np.array([SNR(v) for v in tempprof.values]).clip(0)
                         SNRtemp.ix[i]=SNRprof
                 else:   
-                    tempdat=dset[n].div((dset[n].columns.values/1000.0)**2,axis=1)  #other datasets have been r-squared corrected, and this effect must be cancelled out
+                    tempdat=dset[n].div((dset[n].columns.values)**2,axis=1)  #other datasets have been r-squared corrected, and this effect must be cancelled out
                     stdarray=pan.DataFrame(genfilt(tempdat,np.std,winsize),index=tempdat.index,
                                            columns=tempdat.columns)
                     meanarray=pan.DataFrame(genfilt(tempdat,np.mean,winsize),index=tempdat.index,
@@ -1332,21 +1331,25 @@ def get_files(titlestring,filetype = ('.txt','*.txt')):
     if filenames == "": 
         print "You didn't open anything!"  
         return
-        
-    if isinstance(filenames,list) or isinstance(filenames,tuple): return filenames
-
-    #http://docs.python.org/library/re.html
-    #the re should match: {text and white space in brackets} AND anynonwhitespacetokens
-    #*? is a non-greedy match for any character sequence
-    #\S is non white space
-
-    #split filenames string up into a proper python list
-    result = re.findall("{.*?}|\S+",filenames)
-
-    #remove any {} characters from the start and end of the file names
-    result = [ re.sub("^{|}$","",i) for i in result ]     
     
     root.destroy()
+    
+    if isinstance(filenames,list):
+        result = filenames   
+    elif isinstance(filenames,tuple): 
+        result = list(filenames)
+    else:
+        #http://docs.python.org/library/re.html
+        #the re should match: {text and white space in brackets} AND anynonwhitespacetokens
+        #*? is a non-greedy match for any character sequence
+        #\S is non white space
+    
+        #split filenames string up into a proper python list
+        result = re.findall("{.*?}|\S+",filenames)
+    
+        #remove any {} characters from the start and end of the file names
+        result = [ re.sub("^{|}$","",i) for i in result ] 
+    result.sort()
     return result
 
 def resample_cols(dfin,newcols,verbose=False,method='interp'):
@@ -1390,7 +1393,8 @@ def resample_cols(dfin,newcols,verbose=False,method='interp'):
         
     
 def MPLtoHDF(filename, appendflag = 'False'):
-    
+    #not updated recently - do not use as is!
+    #07/07/2015
     h5filename = filename.split('.')[0]+'_proc.h5'
 
     with open(filename,'rb') as binfile:
@@ -1676,7 +1680,7 @@ def SNR_mask_depol(mplin,**kwargs):
     datatype=kwargs.get('datatype','NRB')
     
     if recalc or not mplin.SNR:
-        mplin = mplin.calculate_SNR(bg_alt,numprofs,datatype=[datatype])
+        mplin = mplin.calculate_SNR(bg_alt,numprofs,datatypes=[datatype])
   
     #start by creating mask where areas that fall below SNRthreshold are zeroed out
     SNRmask = mplin.SNR[datatype][0]>=SNRthreshold
@@ -1691,30 +1695,42 @@ def SNR_mask_depol(mplin,**kwargs):
         mplout.depolrat[0].replace(0,nopassval,inplace=True)
     return mplout 
 
-def SNR_mask_colors(mplin,**kwargs):
+def SNR_mask_scene(mplin,**kwargs):
     
     SNRthreshold=kwargs.get('SNRthreshold',3)
     numprofs=kwargs.get('numprofs',1)
     bg_alt=kwargs.get('bg_alt',None)
-    nopassval=kwargs.get('nopassval',-9999)
     inplace=kwargs.get('inplace',False)
     recalc=kwargs.get('recalc',False)
     datatype=kwargs.get('datatype','NRB')
+    nopassval=kwargs.get('nopassval',None)
     
-    if recalc or not mplin.SNR:
-        mplin = mplin.calculate_SNR(bg_alt,numprofs,datatype=[datatype])
+    if nopassval is None:
+        nopassval={'Base':np.nan,
+                   'Top':np.nan,
+                   'Lidar_Ratio':0.0,
+                   'Delta':0.0,
+                   'Type':'Insufficient Signal',
+                   'Sub-Type':'Insufficient Signal',
+                   'Depol':0.0,
+                   'colormask':9}
+    
+    if recalc or mplin.SNR is None:
+        mplin = mplin.calculate_SNR(bg_alt,numprofs,datatypes=[datatype])
   
     #start by creating mask where areas that fall below SNRthreshold are zeroed out
-    SNRmask = (mplin.SNR[datatype][0]>=SNRthreshold)|(mplin.scenepanel[0]['Type']=='Clear Air')
-    SNRmask.replace(False,np.nan,inplace=True)
+    SNRmask = (mplin.SNR[datatype][0]>=SNRthreshold)#|(mplin.scenepanel[0]['Type']=='Clear Air')
+#    SNRmask.replace(False,np.nan,inplace=True)
+    
     if inplace:
         mplout=mplin
     else:
         mplout=deepcopy(mplin)
            
-    if mplout.scenepanel:        
-        mplout.scenepanel[0]['colormask']=mplin.scenepanel[0]['colormask']*SNRmask
-        mplout.scenepanel[0]['colormask'].replace(np.nan,nopassval,inplace=True)
+    if mplout.scenepanel:
+        for sceneitem,maskval in nopassval.iteritems():
+            mplout.scenepanel[0][sceneitem]=mplin.scenepanel[0][sceneitem]*SNRmask
+            mplout.scenepanel[0][sceneitem].fillna(nopassval,inplace=True)
     return mplout 
     
 def SNR_mask_all(mplin,**kwargs):
@@ -1849,7 +1865,7 @@ def NRB_mask_all(MPLin,**kwargs):
     
     if MPLout.scenepanel:
         for i in MPLout.scenepanel[0].items:
-            MPLout.scenepanel[0][i]=NRB_mask_apply(MPLout.scenepanel[0][i],threshseries)
+            MPLout.scenepanel[0][i]=NRB_mask_apply(MPLout.scenepanel[0][i],threshseries,nopassval='Insufficient Signal')
      
     return MPLout
     

inversiontools.py
@@ -27,7 +27,7 @@ def std_atm(z):
     #Define breakpoints for US Standard atmosphere, with associated altitude,
     #pressure, density, temperature, and dry adiabatic lapse rates
 
-    alt = [0, 11000, 20000, 32000, 47000]
+    alt = [0, 11.000, 20.000, 32.000, 47.000]
     press = [101325,22632.1, 5474.89,868.019,110.906]
     dense = [1.225, 0.36391, 0.08803, 0.01322, 0.00143]
     temp = [288.15, 216.65, 216.65, 228.65, 270.65]
@@ -73,8 +73,8 @@ def molecular(z,wave=532.0):
 
     Outputs:
 
-    beta = backscatter coefficients [1/m*sr]
-    sigma = extinction coefficients [1/m]
+    beta = backscatter coefficients [1/km*sr]
+    sigma = extinction coefficients [1/km]
     """
 
     #calculate temperature and pressure profiles using US Standard atmosphere
@@ -86,7 +86,7 @@ def molecular(z,wave=532.0):
 
     T_s = 288.15  #[K] reference temperature
     P_s = 101325.0  #[Pa] reference pressure
-    N_s = 2.547e25  #[1/m^3]  reference number concentration
+    N_s = 2.547e25  #[1/km^3]  reference number concentration
     gamma = 0.0279 #[unitless] depolarization factor (from Kovalev pg.35)
 
     #calculate reference index of refraction using a polynomial approximation
@@ -111,7 +111,7 @@ def molecular(z,wave=532.0):
     sigma = (8*np.pi**3*(m**2-1)**2*N/(3*N_s**2*(wave*1e-9)**4))*((6+3*gamma)/(6-7*gamma))* \
             (P/P_s)*(T_s/T)
 
-
+    sigma=sigma*1000.0 #convert to 1/km
     #For Rayleigh scattering the extinction to backscatter ratio is 8*pi/3
 
     beta = 3*sigma/(8*np.pi)
@@ -164,15 +164,15 @@ def molprof(z,wave=532.0,T0=1.0,E0=1.0,C=1.0):
     if oldalt==0.0:
         P_z.loc[oldalt] = E0*C*beta_R.iloc[0]*T_total
     else:
-       P_z.loc[oldalt] = E0*C*(oldalt/1000.0)**-2*beta_R.iloc[0]*T_total
+       P_z.loc[oldalt] = E0*C*oldalt**-2*beta_R.iloc[0]*T_total
     
     for alt in z[1:]:
         T_step = np.exp(-2*sigma_R.loc[alt]*(alt-oldalt))
         T_total = T_total*T_step
-        P_z.loc[alt] = (E0*C*beta_R.loc[alt]*T_total)*(alt/1000.0)**-2.0
+        P_z.loc[alt] = (E0*C*beta_R.loc[alt]*T_total)*alt**-2.0
         oldalt=alt
         
-    rsq = [val*(alt/1000.0)**2.0 for val,alt in zip(P_z,z)]
+    rsq = [val*alt**2.0 for val,alt in zip(P_z,z)]
     NRB = [val/E0 for val in rsq]
         
     
@@ -343,7 +343,7 @@ def background_subtract(P_in,back_avg=None,z_min=None,inplace=True):
 
 def calcNRB(P_in,E0=1.0,background=None,inplace=True):
     P_out=background_subtract(P_in,back_avg=background,inplace=inplace)
-    P_out['rsq'] = P_out['vals']*(P_in.index.values/1000.0)**2.0
+    P_out['rsq'] = P_out['vals']*(P_in.index.values)**2.0
     P_out['NRB'] = P_out['rsq']/E0
     
     return P_out
@@ -658,7 +658,7 @@ def klett2(P_in,lrat_in,**kwargs):
     Inputs:
     P_in = a pandas series with values proportional to range squared corrected signal strength and altitude index
     r_m = the reference altitude - maximum altitude for which calculations are done
-            and the point at which the extinction coefficeint is assumed ot be known
+            and the point at which the backscatter coefficeint is assumed ot be known
     lrat_in = a Pandas series with values of particulate only lidar ratio and altitude index
     beta_m = the backscatter coefficient at altitude r_m
         
@@ -670,7 +670,17 @@ def klett2(P_in,lrat_in,**kwargs):
     r_m=kwargs.get('r_m',None)
     wave=kwargs.get('wave',532.0)
     
-    lrat = (1.0/lrat_in).replace(np.inf,0.0) #Klett definition of lidar ratio is backscatter/extintion not the other way round
+    lrat=pan.Series(data=0.0,index=lrat_in.index)
+    
+    grouped=lrat_in.groupby(lrat_in)
+    
+    for name,group in grouped:
+        if name==0.0:
+            continue
+        else:
+            lrat.ix[group.index]=1.0/group
+            
+#    lrat = (1.0/lrat_in).replace(np.inf,0.0) #Klett definition of lidar ratio is backscatter/extintion not the other way round
     lrat_R=3.0/(8.0*np.pi)
     altitudes = P_in.index.values
     
@@ -750,40 +760,40 @@ def invert_profile(profin,lratin,**kwargs):
         moledges.append((temprange[0],temprange[-1]))
     
     alt=profin.index[-1]
-    
-    tempedges=moledges.pop()
-    while True:        
-        if tempedges[0]<alt<=tempedges[1]: 
-            layeralts=profin.ix[tempedges[0]:tempedges[1]].index.values
-            tempmol=molprof(z=layeralts)
-            backscatter.ix[layeralts]=tempmol['beta_R'].values
-            extinction.ix[layeralts]=tempmol['sigma_R'].values
-            alt=layeralts[0]
-        else:
-            try:
-                tempedges=moledges.pop()
-                layeralts=profin.ix[tempedges[1]:alt].index[1:]
-                alt=tempedges[1]
-            except IndexError:
-                layeralts=profin.ix[:alt].index
+    if len(moledges)==0:
+        return backscatter,extinction
+    else:
+        tempedges=moledges.pop()        
+        while True:        
+            if tempedges[0]<alt<=tempedges[1]: 
+                layeralts=profin.ix[tempedges[0]:tempedges[1]].index.values
+                tempmol=molprof(z=layeralts)
+                backscatter.ix[layeralts]=tempmol['beta_R'].values
+                extinction.ix[layeralts]=tempmol['sigma_R'].values
                 alt=layeralts[0]
-                
-            layerprof=profin.ix[layeralts]
-            layerlrat=lratin.ix[layeralts]
-
-            if method=='klett2':
-                tempback,tempext=klett2(layerprof,layerlrat,r_m=layeralts[-1])
+            else:
+                try:
+                    tempedges=moledges.pop()
+                    layeralts=profin.ix[tempedges[1]:alt].index[1:]
+                    alt=tempedges[1]
+                except IndexError:
+                    layeralts=profin.ix[:alt].index
+                    alt=layeralts[0]
+                    
+                layerprof=profin.ix[layeralts]
+                layerlrat=lratin.ix[layeralts]
     
-            backscatter.ix[layeralts]=tempback.values
-            extinction.ix[layeralts]=tempext.values
-            
-        if alt==profin.index[0]:
-            break
+                if method=='klett2':
+                    tempback,tempext=klett2(layerprof,layerlrat,r_m=layeralts[-1])
         
-    return backscatter,extinction
+                backscatter.ix[layeralts]=tempback.values
+                extinction.ix[layeralts]=tempext.values
+                
+            if alt==profin.index[0]:
+                break
+        
+        return backscatter,extinction
             
-    #Step 2: Use Fernald algorithm for molecular sections    
-    #Step 3: Use klett2 for layers
 
 def lrat_tester_full(P_0,**kwargs):
     wave=kwargs.get('wave',532.0)